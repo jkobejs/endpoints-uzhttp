64d4fbbfe20213e20010eb3d6ee3cfb6fd613e8a
jkobejs <josip.grgurica@gmail.com>
2020-05-21 19:00:45 +0200

    typo fix

--- server/src/main/scala/endpoints/uzhttp/server/`Content-Type`.scala
+++ server/src/main/scala/endpoints/uzhttp/server/`Content-Type`.scala
@@ -17,7 +17,7 @@ case class MediaType(
 )
 
 /**
- * Importent MIME types for Web developers
+ * Important MIME types for Web developers
  *
  * https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types
  */
