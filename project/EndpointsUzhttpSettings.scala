cd76bb8d1b3a9ebd0e8947b1371aee13deaeceb0
Scala Steward <me@scala-steward.org>
2020-10-03 08:27:34 +0200

    Update ujson to 1.2.1

--- project/EndpointsUzhttpSettings.scala
+++ project/EndpointsUzhttpSettings.scala
@@ -47,7 +47,7 @@ object EndpointsUzhttpSettings {
   val akkaActorVersion = "2.6.5"
   val scalaTestVersion = "3.1.2"
   val uzhttpVersion    = "0.2.2"
-  val ujsonVersion     = "1.1.0"
+  val ujsonVersion     = "1.2.1"
 
   val macroParadiseDependency = Seq(
     scalacOptions in Compile ++= {
