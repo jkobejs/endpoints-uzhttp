7a37ef3ae666d3e182f35719c279c4995c0541f7
Scala Steward <me@scala-steward.org>
2021-02-24 16:32:28 +0100

    Update akka-http to 10.2.4

--- project/EndpointsUzhttpSettings.scala
+++ project/EndpointsUzhttpSettings.scala
@@ -43,7 +43,7 @@ object EndpointsUzhttpSettings {
   // --- Common dependencies
   val circeVersion     = "0.13.0"
   val endpointsVersion = "1.1.0"
-  val akkaHttpVersion  = "10.2.1"
+  val akkaHttpVersion  = "10.2.4"
   val akkaActorVersion = "2.6.9"
   val scalaTestVersion = "3.1.2"
   val uzhttpVersion    = "0.2.5"
