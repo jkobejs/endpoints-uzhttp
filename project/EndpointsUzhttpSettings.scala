30be010c0a6484833cc4c6d3953db85a3c9d9d5f
Scala Steward <me@scala-steward.org>
2021-05-17 12:50:49 +0200

    Update scala-library to 2.13.6

--- project/EndpointsUzhttpSettings.scala
+++ project/EndpointsUzhttpSettings.scala
@@ -30,8 +30,8 @@ object EndpointsUzhttpSettings {
   )
 
   val `scala 2.12 to latest` = Seq(
-    scalaVersion := "2.13.3",
-    crossScalaVersions := Seq("2.13.3", "2.12.12")
+    scalaVersion := "2.13.6",
+    crossScalaVersions := Seq("2.13.6", "2.12.12")
   )
 
   val noPublishSettings = commonSettings ++ Seq(
